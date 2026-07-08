be30619e812fbbf1f3cc9e08d9e2dffc62b299be
mac <mac@local>
2026-07-08 16:27:05 +0700

    Deploy Handwritten Recognition app

---
+++ README.md
@@ -0,0 +1,12 @@
+---
+title: HandwrittenRecognition
+colorFrom: blue
+colorTo: green
+sdk: docker
+app_port: 7860
+license: mit
+---
+
+# HandwrittenRecognition
+
+Flask app for recognizing handwritten digits.
--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import base64
+import os
 from io import BytesIO
 
 from flask import Flask, jsonify, request, send_from_directory
@@ -81,4 +82,5 @@ def predict_base64():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 7860))
+    app.run(host="0.0.0.0", port=port)
